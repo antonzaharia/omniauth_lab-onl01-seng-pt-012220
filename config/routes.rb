3eca965da49023d44945f344f6517832973973d9
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-21 16:00:00 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'welcome/home'
+  root 'welcome#home'
   # Add your routes here
   match '/auth/:provider/callback', to: 'sessions#create', via: [:get, :post]
 end
